87d3605773063fb4c43636faf0accd603cd9d32a
drummer0417 <hans@androidappfactory.nl>
2017-04-25 00:19:54 +0200

    Create new todoos endpoint

--- server/server.js
+++ server/server.js
@@ -1,16 +1,37 @@
-var express = require('express0');
+var express = require('express');
 var bodyParser = require('body-parser');
 
 var {
     mongoose
 } = require('./db/mongoose');
-var User = require('./db/User');
-var Todo = require('./db/Todo');
+var {
+    User
+} = require('./models/user');
+var {
+    Todo
+} = require('./models/todo');
 
 var port = process.env.PORT || 3000;
 
-
 var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todoos', (req, res) => {
+
+    var todo = new Todo({
+        'text': req.body.text,
+        'completed': req.body.completed
+    });
+
+    todo.save().then((doc) => {
+        console.log(JSON.stringify(todo, undefined, 2));
+        res.send(doc);
+    }, (error) => {
+        console.log(`Error saving document: \n${error}`);
+        res.status(400).send(error);
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
